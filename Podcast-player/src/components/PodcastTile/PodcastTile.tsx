88ec3312d6521b160a6d99df54782ab1e8a374e8
Daniel Cerezo <daniel.c.torrejon@gmail.com>
2024-05-02 15:18:14 +0200

    feat: :lipstick: added styles and lazy loading images

--- Podcast-player/src/components/PodcastTile/PodcastTile.tsx
+++ Podcast-player/src/components/PodcastTile/PodcastTile.tsx
@@ -1,19 +1,31 @@
-import { Podcast } from "../../models/models"
+import { SyntheticEvent, useCallback, useState } from "react";
+import { Podcast } from "../../models/models";
+import skeletonStyle from "../../styles/skeleton.module.css";
+import style from "./PodcastTile.module.css";
 
 interface PodcastTileProps {
-    podcast : Podcast
+  podcast: Podcast;
 }
-export function PodcastTile ({podcast}: PodcastTileProps){
-
-    return (
-        <div>
-            <picture>
-                <source media="(max-width: 30px)" srcSet={podcast.artworkUrl30} />
-                <source media="(max-width: 60px)" srcSet={podcast.artworkUrl60} />
-                <source media="(max-width: 100px)" srcSet={podcast.artworkUrl100} />
-                <img src={podcast.artworkUrl600}/>
-            </picture>
-
-        </div>
-    )
+export function PodcastTile({ podcast }: PodcastTileProps) {
+  const [imgLoading, setImgLoading] = useState(true);
+  const handleLoad = useCallback((event: SyntheticEvent<HTMLImageElement>) => {
+    window.console.log();
+    setImgLoading(false);
+    return event.target;
+  }, []);
+  return (
+    <div className={style.tile}>
+      <picture className={imgLoading ? skeletonStyle.skeleton : ""}>
+        <source media="(max-width: 30px)" srcSet={podcast.artworkUrl30} />
+        <source media="(max-width: 60px)" srcSet={podcast.artworkUrl60} />
+        <source media="(max-width: 100px)" srcSet={podcast.artworkUrl100} />
+        <img
+          loading="lazy"
+          style={imgLoading ? { visibility: "hidden" } : {}}
+          src={podcast.artworkUrl600}
+          onLoad={handleLoad}
+        />
+      </picture>
+    </div>
+  );
 }
--- Podcast-player/src/components/SearchResults/SearchResults.tsx
+++ Podcast-player/src/components/SearchResults/SearchResults.tsx
@@ -1,28 +1,23 @@
 import { PodcastSearchStatesEnum, UseSearchState } from "../../hooks/useSearch";
 import { PodcastTile } from "../PodcastTile/PodcastTile";
+import style from "./SearchResults.module.css";
 interface SearchResultProps {
-    results: UseSearchState,
+  results: UseSearchState;
 }
-export function SearchResults ({results}: SearchResultProps) {
-
-    const loadingView = (<p>Loading ...</p>)
-    const errorView = (<p>There was an error retrieving data</p>)
-    const resultContainerView = (
-        <>
-            {results.result?.map((el) =>{
-                return (<PodcastTile key={el.collectionId} podcast={el} ></PodcastTile>)
-            })}
-        </>
-    )
-    const renderMap : Record<number, JSX.Element>  = {
-        [PodcastSearchStatesEnum.LOADING] : loadingView,
-        [PodcastSearchStatesEnum.ERROR]: errorView,
-        [PodcastSearchStatesEnum.SUCCESS]: resultContainerView
-    }
-    return (
-        <div>
-            {renderMap[results.state]}
-        </div>
-        
-    )
+export function SearchResults({ results }: SearchResultProps) {
+  const loadingView = <p>Loading ...</p>;
+  const errorView = <p>There was an error retrieving data</p>;
+  const resultContainerView = (
+    <>
+      {results.result?.map((el) => {
+        return <PodcastTile key={el.collectionId} podcast={el}></PodcastTile>;
+      })}
+    </>
+  );
+  const renderMap: Record<number, JSX.Element> = {
+    [PodcastSearchStatesEnum.LOADING]: loadingView,
+    [PodcastSearchStatesEnum.ERROR]: errorView,
+    [PodcastSearchStatesEnum.SUCCESS]: resultContainerView,
+  };
+  return <div className={style.area}>{renderMap[results.state]}</div>;
 }
