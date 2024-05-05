9f41b6ec16353ee92bf54ee0093b3170803c2b70
Daniel Cerezo <daniel.c.torrejon@gmail.com>
2024-05-05 13:24:31 +0200

    added new files

--- Podcast-player/src/App.tsx
+++ Podcast-player/src/App.tsx
@@ -1,15 +1,12 @@
-
-import './App.css'
-import { SearchView } from './components/SearchView/SearchView';
+import style from "./App.module.css";
+import { SearchView } from "./components/SearchView/SearchView";
 
 function App() {
-  
-
   return (
-    <>
+    <div className={style.mainLayout}>
       <SearchView></SearchView>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
