36cb5aeee078d32b4483cc6b6ad676cfb9e1ddbb
Daniel Cerezo <daniel.c.torrejon@gmail.com>
2024-05-11 17:24:47 +0200

    chore :recycle: improve lazy

--- Podcast-player/src/App.tsx
+++ Podcast-player/src/App.tsx
@@ -2,11 +2,8 @@ import "./styles/global.css";
 import style from "./App.module.css";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Suspense, lazy } from "react";
-
+const SearchView = lazy(() => import("./components/SearchView/SearchView.tsx"));
 function App() {
-  const SearchView = lazy(
-    () => import("./components/SearchView/SearchView.tsx")
-  );
   return (
     <div className={style.layout}>
       <NavBar containerClass={style.navigation}></NavBar>
