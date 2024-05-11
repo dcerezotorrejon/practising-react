bada64b608a99aa061743929a46441cdb3fb1597
Daniel Cerezo <daniel.c.torrejon@gmail.com>
2024-05-11 17:49:48 +0200

    feat: added button to nav

--- Podcast-player/src/components/NavBar/NavBar.tsx
+++ Podcast-player/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
 import style from "./NavBar.module.css";
+import NavButton from "./NavButton";
+import { Search } from "lucide-react";
 export interface NavBarProps {
   containerClass?: string;
 }
 export function NavBar({ containerClass }: NavBarProps) {
-  return <nav className={`${containerClass} ${style.nav}`}></nav>;
+  return (
+    <nav className={`${containerClass} ${style.nav}`}>
+      <NavButton icon={<Search></Search>} href="" text="Buscar"></NavButton>
+    </nav>
+  );
 }
